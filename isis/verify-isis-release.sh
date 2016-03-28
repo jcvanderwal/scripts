f753c04300fa58e3a77189b952b8824d4008ceac
Jeroen van der Wal <jeroen@stromboli.it>
2016-03-28 12:52:38 +0200

    Updated verify isis script

--- isis/verify-isis-release.sh
+++ isis/verify-isis-release.sh
@@ -5,51 +5,119 @@
 # -Run this script
 # TODO: enhance this script so it will stop when something is broken
 
+FILE=
+
+_usage(){
+    cat << EOF
+    usage: $0 options
+    This script verifies an isis release.
+    OPTIONS:  
+    -h  Show this message
+    -d  <text file> Download  
+    -v  Verify
+    -u  Unpack  
+    -b  Build
+    -a <version> Create archetype
+EOF
+}
+
 _download(){
     while read fil; do
         echo 'Downloading '$fil
-        curl -O $fil
-        curl -O $fil.asc
-    done <release.txt
+        #curl -O $fil
+        #curl -O $fil.asc
+        wget $fil
+        wget $fil.asc
+    done <$FILE
 }
 
 _verify(){
-	for zip in *.zip
-	do 
-		echo 'Verifying '$zip	
-		gpg --verify $zip.asc $zip 
-	done
+    for zip in *.zip
+    do
+        echo 'Verifying '$zip
+        gpg --verify $zip.asc $zip
+    done
 }
 
 _unpack(){
-	echo 'Unpacking '
-	unzip -q '*.zip'
+    echo 'Unpacking '
+    unzip -q '*.zip'
 }
 
 _build(){
-	echo 'Removing Isis from local repo '$module
-	rm -rf ~/.m2/repository/org/apache/isis
-	COUNTER=0
-	for module in ./*/
-	do
-		COUNTER=$[COUNTER+1]
-		if [ $COUNTER -eq 1 ]
-		then
-			cd $module
-			echo 'Building Core '$module
-			mvn clean install -o
-			cd ..
-		else
-			cd $module
-			echo 'Building Module '$module
-			mvn clean install
-			cd ..
-		fi
-	done
+    echo 'Removing Isis from local repo '$module
+    rm -rf ~/.m2/repository/org/apache/isis
+    COUNTER=0
+    for module in ./*/
+    do
+        COUNTER=$[COUNTER+1]
+        if [ $COUNTER -eq 1 ]
+        then
+            cd $module
+            echo 'Building Core '$module
+            mvn dependency:resolve
+            mvn clean install -o
+            cd ..
+        else
+            cd $module
+            echo 'Building Module '$module
+            mvn clean install -o
+            cd ..
+        fi
+    done
 }
 
-# The work starts here 
-_download
-_verify
-_unpack
-_build
+_archetype(){
+mvn archetype:generate \
+    -D archetypeGroupId=org.apache.isis.archetype \
+    -D archetypeArtifactId=simpleapp-archetype \
+    -D archetypeVersion=$VERSION \
+    -D groupId=com.mycompany \
+    -D artifactId=myapp \
+    -D version=1.0-SNAPSHOT \
+    -B \
+    -o
+cd myapp
+mvn clean install
+java -jar webapp/target/myapp-webapp-1.0-SNAPSHOT-jetty-console.jar
+}
+
+
+#Main
+while getopts "hd:vuba:" OPTION; do
+    case $OPTION in
+        h)
+            _usage
+            exit 1
+            ;;
+        d)
+            if [[ -z $OPTARG ]]; then
+                _usage
+                exit 1
+            fi
+            FILE=$OPTARG
+            _download
+            ;;
+        v)
+            _verify
+            ;;
+        u)
+            _unpack
+            ;;
+        b)
+            _build
+            ;;
+        a)
+            if [[ -z $OPTARG ]]; then
+                _usage
+                exit 1
+            fi
+            VERSION=$OPTARG
+            _archetype
+            ;;
+        ?)
+            _usage
+            exit 1
+            ;;
+    esac
+done
